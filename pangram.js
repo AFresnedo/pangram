acc71f620e19e8bfa8edb42d3fa9b97e28f590a1
AFresnedo <AFresnedo@users.noreply.github.com>
2018-07-31 09:04:29 -0700

    finish prototype

--- pangram.js
+++ pangram.js
@@ -5,19 +5,32 @@
   // i mean, for computational sake, stop as soon as array is empty
   // while loop for if array is empty or string is empty?
 
-// TODO is this valid multi-line syntax?
-alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v,
-  w, x, y, z];
 
 function pangram(string) {
+// TODO is this valid multi-line syntax?
+// construct alphabet each call
+  alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v,
+  w, x, y, z];
   // for every char in string
   for (var i = 0; i < string.length; i++) {
     c = string.charAt(i);
-    //  while array is not empty
-    //  if array contains char
-    index = alphabet.indexOf(c);
-    if (index !== -1) {
-      // remove this index from alphabet
+    // while alphabet is not empty
+    while (alphabet !== undefined && alphabet.length !== 0)
+    {
+      //  if array contains char
+      index = alphabet.indexOf(c);
+      if (index !== -1) {
+        // remove this index from alphabet
+        alphabet.splice(i, 1);
+      }
     }
+  }
+  // if alphabet is empty then string is a pangram
+  if (alphabet !== undefined && alphabet.length === 0) {
+    return true;
+  }
+  // else string is not a pangram
+  else {
+    return false;
   }
 }
